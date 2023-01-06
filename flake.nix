8b739ecb2684cbdab7abb662f23dd71a9efbd9b0
Astavie <astavie@pm.me>
2023-01-06 13:25:25 +0100

    odin

--- flake.nix
+++ flake.nix
@@ -10,30 +10,30 @@
     overlays.default = final: prev: {
       odin = prev.odin.overrideAttrs (self: prev: rec {
         version = "dev-2022-10";
-        src = pkgs.fetchFromGitHub {
+        src = final.fetchFromGitHub {
           owner = "odin-lang";
           repo = "Odin";
           rev = version;
           sha256 = "sha256-D6dhsIU2Hm1XQ4G44C0ukJEgiO4tTmZ7CIezWi9CdOY=";
         };
-        LLVM_CONFIG = "${pkgs.llvm.dev}/bin/llvm-config";
+        LLVM_CONFIG = "${final.llvm.dev}/bin/llvm-config";
         postPatch = ''
           sed -i 's/^GIT_SHA=.*$/GIT_SHA=/' build_odin.sh
           patchShebangs build_odin.sh
         '';
       });
-      ols = pkgs.stdenv.mkDerivation {
+      ols = final.stdenv.mkDerivation {
         pname = "ols";
         version = "20221027";
 
-        src = pkgs.fetchFromGitHub {
+        src = final.fetchFromGitHub {
           owner = "DanielGavin";
           repo = "ols";
           rev = "ab9c17b403527bc07d65d5c47ecb25bec423ddac";
           sha256 = "sha256-a6ii6r+zYfO8AJzrL4TWr6Qtze27CZV9MMrA+N8oX+M=";
         };
 
-        buildInputs = [ pkgs.odin ];
+        buildInputs = [ final.odin ];
 
         postPatch = ''
           patchShebangs build.sh
