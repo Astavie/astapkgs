fa88b11ebec6a15ff18c75aeb8942e46362f2877
Astavie <astavie@pm.me>
2023-03-13 22:34:39 +0100

    stardust

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
 
-  outputs = { self, nixpkgs }: let
+  inputs.fenix.url = github:nix-community/fenix;
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, fenix }: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
+      config.allowUnfree = true;
       overlays = [ self.overlays.default ];
     };
+    toolchain = system: fenix.packages.${system}.minimal.toolchain;
   in {
     overlays.default = final: prev: {
       # ---- ODIN ----
@@ -63,8 +68,16 @@
           sha256 = "sha256-rhlxVI2hv66USF52C0VNG6QtJTaK893VRJZOyaxoQAM=";
         };
 
+        nativeBuildInputs = with final; [
+          cmake pkg-config
+        ];
+
         buildInputs = with final; [
-          cmake pkg-config vulkan-headers vulkan-loader systemd ffmpeg eigen x264 avahi pulseaudio glslang shaderc freetype ninja nlohmann_json python3 openxr-loader libGL xorg.libXrandr libdrm libva
+          vulkan-headers vulkan-loader systemd ffmpeg eigen avahi pulseaudio glslang freetype ninja nlohmann_json python3 openxr-loader libGL xorg.libXrandr libdrm libva
+
+          # nvidia
+          cudaPackages.cudatoolkit
+          (linuxPackages.nvidia_x11.override { libsOnly = true; })
         ];
 
         cmakeFlags = [
@@ -87,9 +100,9 @@
 
         installPhase = ''
           mkdir -p $out/bin
-          mkdir -p $out/lib
+          mkdir -p $out/share
           cp server/wivrn-server $out/bin
-          cp openxr_wivrn-dev.json $out/lib
+          cp _deps/monado-build/src/xrt/targets/openxr/libopenxr_wivrn.so $out/share
         '';
       };
 
@@ -101,7 +114,17 @@
 
         echo "Setting WiVRn as the default OpenXR runtime..."
         mkdir -p ~/.config/openxr/1/
-        ln --symbolic --force ${final.wivrn-server}/lib/openxr_wivrn-dev.json ~/.config/openxr/1/active_runtime.json
+        ln --symbolic --force ${final.writeTextFile {
+          name = "openxr_wivrn-dev.json";
+          text = ''
+            {
+                "file_format_version": "1.0.0",
+                "runtime": {
+                    "library_path": "${final.wivrn-server}/share/libopenxr_wivrn.so"
+                }
+            }
+          '';
+        }} ~/.config/openxr/1/active_runtime.json
 
         echo "Put on your Quest and authorize this machine to install the WiVRn client!"
         read -p '(press enter when done)'
@@ -109,6 +132,44 @@
         echo 'Installing client...'
         ${final.android-tools}/bin/adb install -r "$client_file"
       '';
+
+      stardust-xr-server = (final.makeRustPlatform {
+        cargo = toolchain final.system;
+        rustc = toolchain final.system;
+      }).buildRustPackage rec {
+        pname = "stardust-xr-server";
+        version = "20230313";
+
+        src = final.fetchFromGitHub {
+          owner = "StardustXR";
+          repo = "server";
+          rev = "643c4959fb3810e52c381ff633f14a848624151f";
+          sha256 = "sha256-E5QH7bUdN8zscRYUsDbX3qKbxhhoBbIfrG4fF+a3WO8=";
+        };
+
+        cargoSha256 = "sha256-u9Nygffa6/vKI+GIQVyWo/GpxVTOg/xI7rLY388KatU=";
+
+        postPatch = ''
+          sk=/build/stardust-xr-server-${version}-vendor.tar.gz/stereokit-sys/StereoKit
+          mkdir -p $sk/build/cpm
+          cp ${final.fetchurl {
+            url = "https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.32.2/CPM.cmake";
+            hash = "sha256-yDHlpqmpAE8CWiwJRoWyaqbuBAg0090G8WJIC2KLHp8=";
+          }} $sk/build/cpm/CPM_0.32.2.cmake
+        '';
+
+        CPM_SOURCE_CACHE = "./build";
+
+        nativeBuildInputs = with final; [
+          cmake pkg-config llvmPackages.libcxxClang
+        ];
+
+        buildInputs = with final; [
+          openxr-loader libGL mesa xorg.libX11 fontconfig libxkbcommon
+        ];
+
+        LIBCLANG_PATH = "${final.libclang.lib}/lib";
+      };
 
       # ---- OTHER ----
       marksman = final.buildDotnetModule rec {
@@ -128,8 +189,8 @@
         nugetDeps = ./marksman-deps-oIV8Eh.nix;
       };
     };
-    packages.x86_64-linux = {
-      inherit (pkgs) odin ols marksman wivrn-server wivrn-install;
+    packages."x86_64-linux" = {
+      inherit (pkgs) odin ols marksman wivrn-server wivrn-install stardust-xr-server;
     };
   };
 }
