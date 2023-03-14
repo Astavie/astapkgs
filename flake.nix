2150b0b4713d2972b37b0884dc0c56554cd32c66
Astavie <astavie@pm.me>
2023-03-14 20:50:51 +0100

    remove stardust

--- flake.nix
+++ flake.nix
@@ -131,44 +131,6 @@
         ${final.android-tools}/bin/adb install -r "$client_file"
       '';
 
-      stardust-xr-server = (final.makeRustPlatform {
-        cargo = toolchain final.system;
-        rustc = toolchain final.system;
-      }).buildRustPackage rec {
-        pname = "stardust-xr-server";
-        version = "20230313";
-
-        src = final.fetchFromGitHub {
-          owner = "StardustXR";
-          repo = "server";
-          rev = "643c4959fb3810e52c381ff633f14a848624151f";
-          sha256 = "sha256-E5QH7bUdN8zscRYUsDbX3qKbxhhoBbIfrG4fF+a3WO8=";
-        };
-
-        cargoSha256 = "sha256-u9Nygffa6/vKI+GIQVyWo/GpxVTOg/xI7rLY388KatU=";
-
-        postPatch = ''
-          sk=/build/stardust-xr-server-${version}-vendor.tar.gz/stereokit-sys/StereoKit
-          mkdir -p $sk/build/cpm
-          cp ${final.fetchurl {
-            url = "https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.32.2/CPM.cmake";
-            hash = "sha256-yDHlpqmpAE8CWiwJRoWyaqbuBAg0090G8WJIC2KLHp8=";
-          }} $sk/build/cpm/CPM_0.32.2.cmake
-        '';
-
-        CPM_SOURCE_CACHE = "./build";
-
-        nativeBuildInputs = with final; [
-          cmake pkg-config llvmPackages.libcxxClang
-        ];
-
-        buildInputs = with final; [
-          openxr-loader libGL mesa xorg.libX11 fontconfig libxkbcommon
-        ];
-
-        LIBCLANG_PATH = "${final.libclang.lib}/lib";
-      };
-
       # ---- OTHER ----
       marksman = final.buildDotnetModule rec {
         pname = "marksman";
