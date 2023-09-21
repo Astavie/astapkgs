9919b7b302fe286a12a3f6590b1be39bc44b1848
Astavie <astavie@pm.me>
2023-09-21 14:05:47 +0200

    readd lib package

--- flake.nix
+++ flake.nix
@@ -19,10 +19,31 @@
       import ./discord.nix { inherit nixpkgs; } //
       { };
 
+    system-pkgs = system: import nixpkgs {
+      inherit system;
+      overlays = [ self.overlays.default ];
+    };
+
   in {
 
     overlays.default = pkgs: prev: system-astapkgs prev;
     packages."x86_64-linux" = system-astapkgs (system-nixpkgs "x86_64-linux");
+
+    lib.package = { package ? null, devShell ? null }: systems: {
+      overlays.default = _: pkgs: let
+        pkg = package pkgs;
+      in {
+        ${pkg.pname} = pkg;
+      };
+      packages = builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value.default = package (system-pkgs system);
+      }) systems);
+      devShells = builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value.default = devShell (system-pkgs system);
+      }) systems);
+    };
 
   };
 }
